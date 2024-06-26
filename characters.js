3188a79805376f1f00790a17d801bd9d3f69a7e3
Marko <markogidej@yahoo.com>
2024-06-26 14:29:47 -0400

    Q1 done testing private key hiding

--- characters.js
+++ characters.js
@@ -1,18 +1,21 @@
+const ts = Date.now();
+const publicKey = '10e6807befd8aeb6f15c4e5356806e98';
+const hash = CryptoJS.MD5(ts + privateKey + publicKey);
+
 async function fetchCharacter(characterName) {
-    const ts = Date().now();
-    const publicKey = '10e6807befd8aeb6f15c4e5356806e98';
-    const privateKey = '';
-    const hash = ts + publicKey + privateKey;
     const response = await fetch(`https://gateway.marvel.com/v1/public/characters?name=${characterName}&ts=${ts}&apikey=${publicKey}&hash=${hash}`);
     const characterData = await response.json();
-    console.log(characterData);
-    return characterData;
+    return characterData.data.results[0];
 }
 
-document.getElementById("nameField").addEventListener("onclick", async () => {
+document.getElementById("searchButton").addEventListener("click", async () => {
     const inputName = document.getElementById("nameField").value;
-    const characterData = fetchCharacter(inputName);
-
-    const infoSection = document.getElementById("characterInfo");
-    infoSection.innerHTML = characterData;
+    try {
+        const characterData = await fetchCharacter(inputName);
+        const infoSection = document.getElementById("characterInfo");
+        infoSection.innerHTML = JSON.stringify(characterData);
+    }
+    catch (error) {
+        console.error("this is the error", error);
+    }
 });
